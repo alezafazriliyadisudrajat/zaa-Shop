5cea7483e8b89ca6841b5850bcf67b834b398b23
alezafazriliyadisudrajat <alezafazriliyadisudrajat@gmail.com>
2024-11-02 09:49:13 +0700

    update tampilan

---
+++ client/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Products from './assets/components/Products';
+import Layout from './layouts/Layouts';
+
+function App() {
+  return (
+    <Layout>
+      <Products></Products>
+    </Layout>
+  );
+}
+
+export default App;
---
+++ client/src/assets/components/Products.jsx
@@ -0,0 +1,287 @@
+
+
+
+const Products = () => {
+    return (
+        <main>
+        <section className="text-gray-600 body-font">
+          <div className="container px-5 py-24 mx-auto">
+            {/* Bagian Produk yang Juga Dibeli */}
+            <div className="bg-white">
+              <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+                <h2 className="text-2xl font-bold tracking-tight text-gray-900">Customers also purchased</h2>
+                <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe4UQ0eNVDIT4CbIpcKC0iklqLjvN54wpIdA&s"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Basic Tee
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Black</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">$35</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://down-id.img.susercontent.com/file/id-11134207-7r98s-lthac4cxip6e12"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Jaket Capung
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Black</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp360.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//97/MTA-91731328/no_brand_sepatu_converse_all_star_chuck_taylor_70s_high_lylac_taro_full07_cnfvytq5.jpg"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Converse High Lylac
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Taro</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp1.200.000</p>
+                    </div>
+                  </div>
+                  
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://images.voila.id/pr:sharp/rs:fit:2048/plain/https%3A%2F%2Fassets.voila.id%2Fvoila%2Fimages%2Fproduct%2Fadidas%2F1product-IE7011-Xms-2024-02-16T1631250700.jpeg@webp"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Adidas Samba
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Silver Green</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp1.800.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://www.eqvvs.co.uk/cdn/shop/files/5056746201077_2_300x.jpg?v=1727956208"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            marshal artist
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Black</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp200.000</p>
+                    </div>
+                  </div>
+                  
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://s3.bukalapak.com/img/33960734692/large/data.jpg"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Caliburn Koko AK2
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Black</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp400.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://images.tokopedia.net/img/cache/700/VqbcmM/2020/12/8/7f9abfa4-4f82-4fa5-8dc0-f4f6052c45e7.jpg"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Crewneck Cewe Rose Hitam
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Black</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp58.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://cdn.idntimes.com/content-images/post/20230330/citizen-watch-dceae051e666a47668bf06a67646b86c.png"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Wave Original
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Silver</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp280.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRd4eDch4LdzTsbfDGEiNg_HvtGg1uv8gimqw&s"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            House Of Smith
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Pink</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp200.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://timekingdom.co.id/cdn/shop/products/17ea27fd2671647595826fe8166d6b5c@2x.jpg?v=1671435983"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                           Lorenzo Wanita
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Purple</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp270.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://www.matahari.com/media/catalog/product/1/5/15415544_1.jpg?optimize=medium&fit=bounds&height=621&width=485"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Cargo Wanita
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Gray</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp130.000</p>
+                    </div>
+                  </div>
+
+                  <div className="group relative">
+                    <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+                      <img
+                        src="https://www.emoline.id/wp-content/uploads/2023/08/BRAND-EMOLINE-BG-WHITE-_0-223_1-1.jpg"
+                        alt="Front of men's Basic Tee in black."
+                        className="h-full w-full object-cover object-center lg:h-full lg:w-full"
+                      />
+                    </div>
+                    <div className="mt-4 flex justify-between">
+                      <div>
+                        <h3 className="text-sm text-gray-700">
+                          <a href="#">
+                            <span aria-hidden="true" className="absolute inset-0"></span>
+                            Cargo Short
+                          </a>
+                        </h3>
+                        <p className="mt-1 text-sm text-gray-500">Black</p>
+                      </div>
+                      <p className="text-sm font-medium text-gray-900">Rp130.000</p>
+                    </div>
+                  </div>
+                  {/* Tambahkan lebih banyak produk sesuai kebutuhan */}
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </main>
+    )
+}
+
+export default Products;
